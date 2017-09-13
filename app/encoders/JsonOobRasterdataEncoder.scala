74e93e6f1d60567ebed9b41bd47a1f7905e67718
Jochen Lutz <jlu@gmx.de>
2017-09-13 14:57:38 +0200

    JsonOobRasterdataEncoder implemented

---
+++ app/encoders/JsonOobRasterdataEncoder.scala
@@ -0,0 +1,25 @@
+package encoders
+import java.io.File
+
+import org.apache.kafka.clients.producer.ProducerRecord
+import play.api.libs.json.Json
+
+/**
+  * Created by Jochen Lutz on 2017-09-13.
+  */
+class JsonOobRasterdataEncoder(aTopic: String, aBaseUrl: String) extends BaseRasterdataEncoder(aTopic) {
+  val kafkaTopic = aTopic + ".oob"
+  val baseUrl = aBaseUrl
+
+  override def encodeMessage(file: File): ProducerRecord[String, String] = {
+    val json = Json.obj(
+      "latitude" -> 0,//TODO
+      "longitude" -> 0,
+      "altitude" -> 0,
+      "filename" -> file.getName,
+      "raster-data-location" -> (baseUrl + file.getPath)
+    )
+
+    return new ProducerRecord(kafkaTopic, "raster-data-test-2", json.toString())
+  }
+}
--- app/encoders/RasterdataEncoderFactory.scala
+++ app/encoders/RasterdataEncoderFactory.scala
@@ -4,7 +4,8 @@ package encoders
   * Created by Jochen Lutz on 2017-09-11.
   */
 object RasterdataEncoderFactory {
-  def apply(serializerType: String, topic: String): BaseRasterdataEncoder = serializerType match {
+  def apply(serializerType: String, topic: String, baseUrl: String): BaseRasterdataEncoder = serializerType match {
     case "json-inline" => new JsonInlineRasterdataEncoder(topic)
+    case "json-oob" => new JsonOobRasterdataEncoder(topic, baseUrl)
   }
 }
